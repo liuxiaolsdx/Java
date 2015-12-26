a1be97a8175581130ecb61642cf9122fdf7f5dca
liuxiaolsdx <liuxiaolsdx@gmail.com>
2015-12-26 22:46:57 +0800

    add question: FindInPartiallySortedMatrix and test case

---
+++ test/ArrayFindTest.java
@@ -0,0 +1,28 @@
+/**
+ * Created by Sean on 15/12/26.
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ArrayFindTest {
+    private int[][] A = {{1, 2, 8, 9},
+            {2, 4, 9, 12},
+            {4, 7, 10, 13},
+            {6, 8, 11, 15}};
+
+    @Test
+    public void findTest() {
+        assertTrue(ArrayFind.find(A, A.length, A[0].length, 1));
+        assertTrue(ArrayFind.find(A, A.length, A[0].length, 15));
+        assertTrue(ArrayFind.find(A, A.length, A[0].length, 7));
+
+        assertFalse(ArrayFind.find(A, A.length, A[0].length, 20));
+        assertFalse(ArrayFind.find(A, A.length, A[0].length, 0));
+        assertFalse(ArrayFind.find(A, A.length, A[0].length, 5));
+
+        int[][] B = null;
+        assertFalse(ArrayFind.find(B, 0, 0, 7));
+    }
+}
