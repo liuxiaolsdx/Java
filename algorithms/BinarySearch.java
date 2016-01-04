e24e13c4af05ab9f03029ddc5161a9560d35922c
liuxiaolsdx <liuxiaolsdx@gmail.com>
2016-01-04 22:29:33 +0800

    add BinarySearch

---
+++ algorithms/BinarySearch.java
@@ -0,0 +1,53 @@
+import com.sun.istack.internal.NotNull;
+
+/**
+ * Binary Search
+ * Created by Sean on 16/1/4.
+ */
+public class BinarySearch {
+    /**
+     * Recursive
+     */
+    @NotNull
+    public static boolean search(int[] a, int low, int high, int num) {
+        if (null == a) {
+            throw new IllegalArgumentException("The array is null");
+        }
+
+        if (low > high) {
+            return false;
+        }
+
+        int mid = low + (high - low) / 2;//avoid int overflow
+
+        if (num < a[mid]) {
+            return search(a, low, mid - 1, num);
+        } else if (num > a[mid]) {
+            return search(a, mid + 1, high, num);
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * loop
+     */
+    public static boolean search2(int[] a, int low, int high, int num) {
+        if (null == a) {
+            throw new IllegalArgumentException("The array is null");
+        }
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (num < a[mid]) {
+                high = mid - 1;
+            } else if (num > a[mid]) {
+                low = mid + 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ test/BinarySearchTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * BinarySearchTest
+ * Created by Sean on 16/1/4.
+ */
+public class BinarySearchTest {
+    private int[] a = {1, 3, 10, 13, 16, 20, 24, 37, 50, 77};
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testBinarySearch() {
+        assertTrue(BinarySearch.search(a, 0, a.length - 1, 1));
+        assertTrue(BinarySearch.search(a, 0, a.length - 1, 77));
+        assertTrue(BinarySearch.search(a, 0, a.length - 1, 16));
+        assertFalse(BinarySearch.search(a, 0, a.length - 1, 11));
+        assertFalse(BinarySearch.search(null, 0, a.length - 1, 13));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testBinarySearch2() {
+        assertTrue(BinarySearch.search2(a, 0, a.length - 1, 1));
+        assertTrue(BinarySearch.search2(a, 0, a.length - 1, 77));
+        assertTrue(BinarySearch.search2(a, 0, a.length - 1, 16));
+        assertFalse(BinarySearch.search2(a, 0, a.length - 1, 11));
+        assertFalse(BinarySearch.search2(null, 0, a.length - 1, 13));
+    }
+
+}
