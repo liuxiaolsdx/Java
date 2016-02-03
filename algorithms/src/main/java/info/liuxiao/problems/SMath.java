67fa2cbd3b4d60ceb6b843e662eeb93c96966c7b
liuxiaolsdx <liuxiaolsdx@gmail.com>
2016-02-03 21:43:11 +0800

    refactor: effective calculate integer power

--- algorithms/src/main/java/info/liuxiao/problems/SMath.java
+++ algorithms/src/main/java/info/liuxiao/problems/SMath.java
@@ -16,9 +16,33 @@ public class SMath {
 
         int absExponent = exponent < 0 ? -exponent : exponent;
 
-        for (int i = 0; i < absExponent; i++) {
+        //for (int i = 0; i < absExponent; i++) {
+        //    result *= base;
+        //}
+        result = powerWithUnsignedExponet(base, absExponent);
+        return exponent < 0 ? 1 / result : result;
+    }
+
+    /**
+     * 一个数的正整数次方可以用二分法来降低运算时间,从O(n) -> O(logn)
+     * 对于a^x,当x为偶数时,即a^(x/2) * a^(x/2);当x为奇数时,即a^[(x-1)/2] * a^[(x-1)/2] * a
+     * @param base 底
+     * @param exponent 指数
+     * @return 结果
+     */
+    public static double powerWithUnsignedExponet(double base, int exponent) {
+        if (exponent == 0)
+            return 1;
+        if (exponent == 1)//递归结束条件
+            return base;
+
+        double result = powerWithUnsignedExponet(base, exponent/2);
+
+        result *= result;
+
+        if (exponent%2 != 0) {
             result *= base;
         }
-        return exponent < 0 ? 1 / result : result;
+        return result;
     }
 }
