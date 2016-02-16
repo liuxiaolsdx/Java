1b5349e9616c3ec29d5f5f03306ccc501df0dc3a
liuxiaolsdx <liuxiaolsdx@gmail.com>
2016-02-16 23:58:05 +0800

    closed: question 17

--- algorithms/src/main/java/info/liuxiao/SinglyLinked.java
+++ algorithms/src/main/java/info/liuxiao/SinglyLinked.java
@@ -272,4 +272,12 @@ public class SinglyLinked<T> {
      * 题目17: 合并两个排序的链表
      * 输入两个递增排序的链表,合并这两个链表并使新链表中的结点仍然是按照递增排序的.
      */
+    public void merge(SinglyLinked ls1, SinglyLinked ls2) {
+        if (ls1.size == 0 || ls2.size == 0 || ls1.head == null || ls2.head == null) {
+            return;
+        }
+        Node ls1Index = ls1.head;
+        Node ls2Index = ls2.head;
+        //TODO: closed due to can't compare Node.Date
+    }
 }
