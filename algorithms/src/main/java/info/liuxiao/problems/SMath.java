e9f6e645a7c0f97ef091bcdb90fbe9dfab842474
liuxiaolsdx <liuxiaolsdx@gmail.com>
2016-02-22 22:24:46 +0800

    add: new test case

--- algorithms/src/main/java/info/liuxiao/problems/SMath.java
+++ algorithms/src/main/java/info/liuxiao/problems/SMath.java
@@ -121,10 +121,13 @@ public class SMath {
         int[][] matrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};//1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
         int[][] matrixVertical = {{1,2,3},{4,5,6},{7,8,9},{10,11,12},{13,14,15}};//1 2 3 6 9 12 15 14 13 10 7 4 5 8 11
         int[][] matrixHorizontal = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};//1 2 3 4 8 12 11 10 9 5 6 7
+        int[][] matrix1 = {{1,2,3},{4,5,6},{7,8,9}};//1 2 3 6 9 8 7 4 5
         SMath.printMatrixClockwisely(matrix);
         System.out.println();
         SMath.printMatrixClockwisely(matrixVertical);
         System.out.println();
         SMath.printMatrixClockwisely(matrixHorizontal);
+        System.out.println();
+        SMath.printMatrixClockwisely(matrix1);
     }
 }
