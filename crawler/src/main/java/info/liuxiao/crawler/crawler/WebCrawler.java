211a4aa94b1180fc5032e6eba73d864af7dbdc85
liuxiaolsdx <liuxiaolsdx@gmail.com>
2016-02-26 22:53:27 +0800

    add: WebCrawler

---
+++ crawler/src/main/java/info/liuxiao/crawler/crawler/WebCrawler.java
@@ -0,0 +1,9 @@
+package info.liuxiao.crawler.crawler;
+
+/**
+ * WebCrawler class in the Runnable class that is executed by each crawler thread.
+ * Created by Sean on 16/2/26.
+ */
+public class WebCrawler implements Runnable{
+    public void run() {}
+}
