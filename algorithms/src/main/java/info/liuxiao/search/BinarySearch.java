9b094e4289840d60b59195e4903cf0b4d2396181
liuxiaolsdx <liuxiaolsdx@gmail.com>
2021-02-24 20:07:54 +0800

    [M] BinarySearch

--- algorithms/src/main/java/info/liuxiao/search/BinarySearch.java
+++ algorithms/src/main/java/info/liuxiao/search/BinarySearch.java
@@ -52,4 +52,32 @@ public class BinarySearch {
         }
         return false;
     }
+
+    /**
+     * 给定一个n个元素有序的（升序）整型数组nums 和一个目标值target ，写一个函数搜索nums中的 target，如果目标值存在返回下标，否则返回 -1。
+     *
+     */
+    public static int search3(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (target < nums[mid]) {
+                right = mid - 1;
+            } else if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1, 0, 3, 5, 9, 12};
+        System.out.println(BinarySearch.search3(nums, 9));
+    }
+
 }
